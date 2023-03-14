9282a97ce34296522dad7a1a010bd0ef3908da82
Oscar <eloskr16@gmail.com>
2023-03-14 13:58:08 +0100

    starting navbar

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const openMenuButton = document.getElementById("openMenu");
+const closeMenuButton = document.getElementById("closeMenu");
+const menuHeader = document.getElementsByClassName("header__content-menu");
+
+openMenuButton.addEventListener("click",toggleMenu)
+
+closeMenuButton.addEventListener("click",toggleMenu);
+
+function toggleMenu(){
+    openMenuButton.classList.toggle("active");
+    closeMenuButton.classList.toggle("active");
+    menuHeader.classList.toggle("active");
+}
