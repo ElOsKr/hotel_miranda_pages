e0c32d2a9af36b40ccfb6240f9ea27007c870fe7
Oscar <eloskr16@gmail.com>
2023-03-14 14:20:13 +0100

    js state code

--- js/index.js
+++ js/index.js
@@ -1,13 +1,11 @@
-const openMenuButton = document.getElementById("openMenu");
-const closeMenuButton = document.getElementById("closeMenu");
-const menuHeader = document.getElementsByClassName("header__content-menu")[0];
-
-openMenuButton.addEventListener("click",toggleMenu)
-
-closeMenuButton.addEventListener("click",toggleMenu);
+document.addEventListener("readystatechange", cargarEventos, false);
+function cargarEventos(){
+    document.getElementById("openMenu").addEventListener("click",toggleMenu)
+    document.getElementById("closeMenu").addEventListener("click",toggleMenu);
+}
 
 function toggleMenu(){
-    openMenuButton.classList.toggle("active");
-    closeMenuButton.classList.toggle("active");
-    menuHeader.classList.toggle("active");
+    document.getElementById("openMenu").classList.toggle("active");
+    document.getElementById("closeMenu").classList.toggle("active");
+    document.getElementsByClassName("header__content-menu")[0].classList.toggle("active");
 }
