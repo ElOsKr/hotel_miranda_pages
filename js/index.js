152a54cc648b5d0512abfc808d5e942526eeef96
Oscar <eloskr16@gmail.com>
2023-03-21 11:36:42 +0100

    sliders index done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,64 @@
 document.addEventListener("readystatechange", cargarEventos, false);
 function cargarEventos(){
-    document.getElementById("openMenu").addEventListener("click",toggleMenu)
+    document.getElementById("openMenu").addEventListener("click",toggleMenu);
     document.getElementById("closeMenu").addEventListener("click",toggleMenu);
+
+    const swiper = new Swiper('.swiper',{
+        direction: 'horizontal',
+        loop: true,
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        autoplay:{
+            delay: 4000,
+        }
+    })
+
+    const swiperFeatures = new Swiper('.swiper-features',{
+        direction: 'horizontal',
+        loop: true,
+
+        pagination:{
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+
+        autoplay:{
+            delay: 4000,
+        }
+    })
+
+    const swiperMenu = new Swiper('.swiper-menu',{
+        direction: 'horizontal',
+        loop: true,
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // autoplay:{
+        //     delay: 4000,
+        // }
+    })
+
+    const swiperFoodImg = new Swiper('.swiper-foodImg',{
+        direction: 'horizontal',
+        loop: true,
+
+        pagination:{
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+
+        autoplay:{
+            delay: 4000,
+        }
+    })
+
 }
 
 function toggleMenu(){
