c5ce02ca262db2912717d435516dabf40da508bc
Oscar <eloskr16@gmail.com>
2023-03-23 14:51:39 +0100

    rooms desktop done

--- js/roomDetails.js
+++ js/roomDetails.js
@@ -3,11 +3,18 @@ function cargarEventos(){
     const swiperRelatedRooms = new Swiper('.swiper-relatedRooms',{
         direction: 'horizontal',
         loop: true,
+        spaceBetween:20,
 
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+        breakpoints:{
+            1000:{
+                loop: false,
+                slidesPerView: 2,
+            }
+        }
     })
 
 }
